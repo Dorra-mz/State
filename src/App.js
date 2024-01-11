d6271891602873e2cf2eb4c459acb156d4fdc474
Dorra-mz <dorramziou@gmail.com>
2024-01-11 17:33:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'John Snow',
+        bio: 'A short bio about John Snow.',
+        imgSrc: 'https://th.bing.com/th/id/R.bdf26c72e5ea48a9ca0952317a0cec73?rik=mugWTFHX7Hb5Jw&pid=ImgRaw&r=0',
+        profession: 'Software Developer',
+      },
+      show: false,
+      mountTime: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ mountTime: new Date() });
+
+    this.intervalId = setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  toggleShow() {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  }
+
+  render() {
+    const { person, show, mountTime } = this.state;
+
+    return (
+      <div style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', padding: '20px', color: 'white', backgroundColor: 'blue' }}>
+        <h1>React Class Component</h1>
+        <button
+          style={{
+            padding: '10px',
+            margin: '10px',
+            fontSize: '16px',
+            color: 'white',
+            backgroundColor: 'black',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+            
+          }}
+          onClick={() => this.toggleShow()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Toggle Profile
+        </button>
+
+        {show && (
+          <div
+            style={{
+              margin: '20px',
+              border: '1px solid black',
+              padding: '10px',
+              borderRadius: '8px',
+              backgroundColor: 'white',
+            }}
+          >
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt={person.fullName} style={{ maxWidth: '100%', borderRadius: '4px' }} />
+            <p style={{ fontWeight: 'bold', marginTop: '10px' }}>Profession: {person.profession}</p>
+          </div>
+        )}
+
+        <p style={{ marginTop: '20px', color: 'black' }}>
+          Time interval since mount: {mountTime && Math.floor((new Date() - mountTime) / 1000)} seconds
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
+    
+           
